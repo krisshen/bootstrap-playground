96f88ca4699adfce4d6f22c9b9026f5168d741f0
krisshen <kris1126@163.com>
2020-09-19 23:32:30 +1000

    backup and generate random numbers in js

--- loife/js/loife.js
+++ loife/js/loife.js
@@ -1,4 +1,17 @@
+function generateRandomNumbers(size) {
+    let numbers = [];
+    while (numbers.length < size) {
+        let n = Math.floor(Math.random() * 60) + 1;
+        if (numbers.indexOf(n) === -1) numbers.push(n);
+    }
+    return numbers;
+}
+
 $(document).ready(function (){
+
+    let randomNumbers = generateRandomNumbers(8);
+
+    console.log(randomNumbers);
 
     let modal = $('.modal');
 
